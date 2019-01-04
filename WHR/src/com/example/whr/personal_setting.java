52e23188cccfaab569f0ce741ed4cc22e6da6fd3
lunpika <380475003@qq.com>
2019-01-04 14:55:16 +0800

    2

--- WHR/src/com/example/whr/personal_setting.java
+++ WHR/src/com/example/whr/personal_setting.java
@@ -1,6 +1,8 @@
 package com.example.whr;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -26,11 +28,22 @@ public class personal_setting extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.personal_setting);
 		x.view().inject(this);
+		loadInfo();
+	}
+	
+	private void loadInfo() {
+        SharedPreferences sp = getSharedPreferences("user",Context.MODE_PRIVATE);
+        String username = sp.getString("username","");
+        String signature = sp.getString("signature","");
+        et_new_username.setText(username);
+        et_new_signature.setText(signature);
 	}
 	
 	@Event(value = {R.id.btn_confirm})
-	private void onClick() {
+	private void onClick(View v) {
 		// TODO Auto-generated method stub
+		String username = et_new_username.getText().toString();
+		String signature = et_new_signature.getText().toString();
 		
 	}
 
