764e4dd492bdffca33d8b82b3edf0e5f97053fa3
lunpika <380475003@qq.com>
2019-01-04 14:37:48 +0800

    1

--- WHR/src/com/example/whr/personal_setting.java
+++ WHR/src/com/example/whr/personal_setting.java
@@ -1,5 +1,6 @@
 package com.example.whr;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,19 +10,28 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 
 import org.xutils.x;
+import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 
-public class personal_setting extends Fragment{
+public class personal_setting extends Activity{
 	
 	@ViewInject(R.id.et_new_username)
 	EditText et_new_username;
+	@ViewInject(R.id.et_new_signature)
+	EditText et_new_signature;
 	
 	@Override
-	public View onCreateView(LayoutInflater inflater,
-			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		View view= inflater.inflate(R.layout.personal_setting, null);
-		return view;
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.personal_setting);
+		x.view().inject(this);
+	}
+	
+	@Event(value = {R.id.btn_confirm})
+	private void onClick() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
